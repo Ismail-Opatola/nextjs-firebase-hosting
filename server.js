548d32d058bebce150797d11bea887aaddb76322
Ismail Opatola <opatolamails@gmail.com>
2021-06-05 14:53:52 +0100

    integrate next with cloud function

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const next = require("next");
+const config = require("./next.config");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+/**
+ * Initialize firbase-admin sdk
+ * ----------------------------
+ */
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+/**
+ * Integrate Next routes with Cloud function
+ * -----------------------------------------
+ */
+const dev = process.env.NODE_ENV !== "production";
+const app = next({
+  dev,
+  // the absolute directory from the package.json file that initialises this module
+  // IE: the absolute path from the root of the Cloud Function
+  conf: config,
 });
+const handle = app.getRequestHandler();
+
+const server = functions.https.onRequest((request, response) => {
+  // log the page.js file or resource being requested
+  console.log("File: " + request.originalUrl);
+  return app.prepare().then(() => handle(request, response));
+});
+
+exports.nextjs = {server};
+
+/**
+ * Other Cloud functions
+ * ---------------------
+ */
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
